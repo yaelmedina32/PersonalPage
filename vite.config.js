b3bddb8c0000c2b0c790f262ac64cb740cb66e7c
yaelmedina32 <yael_medina9@outlook.com>
2025-07-31 16:19:19 -0600

    Se creo un json 'para verce

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  // Configuración para el servidor de desarrollo
+  server: {
+    historyApiFallback: true,
+  },
 })
